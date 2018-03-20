3cecea9ecf791adc27648a0392535770988eb1b6
Phenix66 <34311559+Phenix66@users.noreply.github.com>
2018-03-20 14:53:43 -0400

    Language mode option
    Added prompt to ask which language mode is preferred.

--- Scripts/Securing_PowerShell.ps1
+++ Scripts/Securing_PowerShell.ps1
@@ -1,6 +1,6 @@
 <#
 Windows PowerShell is an extremely powerful utility built into all distributions of Windows 7/2008 R2 and later.
-Because of this it has became a popular tool among attackers and can be very dangerous if not properly secured.
+Because of this, it has became a popular tool among attackers and can be very dangerous if not properly secured.
 #>
 
 # Check for admin
@@ -10,14 +10,10 @@ function Test-IsAdmin {
 
 # Exit the script if it is not running under admin rights
 If (!(Test-ISAdmin)){
-  Write-Host "This script must be ran as admin. Please re-run from an admin PowerShell session"
-  $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-  exit
+    Write-Host "This script must be ran as admin. Please re-run from an admin PowerShell session"
+    $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    exit
 }
-
-# Enable Constrained Language Mode, removes access to advanced features such as API calls and COM access
-# Can also be done through Group Policy at Computer Configuration\Preferences\Windows Settings\Environment
-[Environment]::SetEnvironmentVariable(‘__PSLockdownPolicy‘, ‘4’, ‘Machine‘)
 
 <# 
 If PowerShell V2 is installed, remove it. This prevents downgrade attacks.
@@ -27,4 +23,23 @@ to remove the files necessary to renable, effectively disabling it altogether.
 $x = Get-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2
 If ($x.State -eq "Enabled"){
     Disable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2
+}
+
+# Set the PowerShell Language mode to what is desired
+$Input = Read-Host @"
+PowerShell Language Modes:
+0 - Full
+1 - Constrained (Prevents most API calls and COM access)
+Please enter the desired PowerShell language mode
+"@
+
+While ("0","1","full","constrained" -notcontains $Input)
+{
+    $Input = Read-Host "Invalid option, please enter 0 or 1 to proceed"
+}
+
+If ($Input -eq "0" -or $Input -eq "full"){
+    write-host "full"
+} ElseIf ($Input -eq "1" -or $Input -eq "constrained"){
+    write-host "constrained"
 }
