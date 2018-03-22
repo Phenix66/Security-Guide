36f4a7f5d74ddc1c8809e2ec7b16f117bd5f8ed9
Phenix66 <34311559+Phenix66@users.noreply.github.com>
2018-03-22 09:24:28 -0400

    Update Disable_SMB_1&2.ps1

--- Scripts/Disable_SMB_1&2.ps1
+++ Scripts/Disable_SMB_1&2.ps1
@@ -4,7 +4,20 @@ to use for an SMB connection in order to provide maximum backwards compatibility
 force Windows to only accept SMB v3 connections and prevent attackers from exploiting old and well known vulnerabilities.
 #>
 
+# Check for admin
+function Test-IsAdmin { 
+([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator") 
+} 
+
+# Exit the script if it is not running under admin rights
+If (!(Test-ISAdmin)){
+    Write-Host "This script must be ran as admin. Please re-run from an admin PowerShell session"
+    $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    exit
+}
+
 # Get SMB v1 config
+Write-Host "Disabling SMB v1..."
 $x = Get-WindowsOptionalFeature –Online –FeatureName SMB1Protocol
 
 # If SMB v1 is enabled, disable it
@@ -13,9 +26,14 @@ If ($x.State -eq "Enabled"){
 }
 
 # Get SMB v2 config
+Write-Host "Disabling SMB v2..."
 $y = Get-SmbServerConfiguration | Select EnableSMB2Protocol
 
 # If SMB v2 is enabled, disable it
 If ($y -Match 'True'){
   Set-SmbServerConfiguration -EnableSMB2Protocol $false
 }
+
+Write-Host "Done."`n
+Write-Host "Press any key to exit..."
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
