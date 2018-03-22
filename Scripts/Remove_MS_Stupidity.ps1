f832ef99981ac215bd1ce6ba13e97c33af3f185c
Phenix66 <34311559+Phenix66@users.noreply.github.com>
2018-03-22 10:06:05 -0400

    Added force-mkdir

--- Scripts/Remove_MS_Stupidity.ps1
+++ Scripts/Remove_MS_Stupidity.ps1
@@ -2,6 +2,7 @@
 Microsoft has built in several "features" that will collect data on user behavior.
 This script attempts to disable as many of these "features" as possible, but updates will sometimes re-enable "features".
 My recommendation is to run this script after first install, then use O&O ShutUp on a regular basis to check configuration.
+https://www.oo-software.com/en/shutup10
 #>
 
 # Check for admin
@@ -16,11 +17,23 @@ If (!(Test-ISAdmin)){
     exit
 }
 
+function force-mkdir($path) {
+    if (!(Test-Path $path)) {
+        #Write-Host "-- Creating full path to: " $path -ForegroundColor White -BackgroundColor DarkGreen
+        New-Item -ItemType Directory -Force -Path $path
+    }
+}
+
 # Remove all "Apps". Retains Calc and the core Store functionality
 Write-Host "Removing bloatware..."
 Get-AppxPackage -AllUsers | Where-Object {$_.name -notlike "calc" -AND $_.name -notlike "store" -AND $_.name `
     -notlike "onenote" -AND $_.name -notlike "NET." -AND $_.name -notlike "VCLibs" -AND $_.name -notlike "Host" `
     -AND $_.name -notlike "AccountsControl"} | Remove-AppxPackage
+
+# Disables the downloading of updates from other PCs
+Write-Host "Disabling Windows Update Peer-2-Peer behavior..."
+force-mkdir "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeliveryOptimization"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeliveryOptimization" -Name "DODownloadMode" -Value 0
 
 # Disable telemetry services
 Write-Host "Nuking Microsoft spy services..."
@@ -31,6 +44,7 @@ If ( Get-Service "dmwappushservice") {
     sc.exe delete "dmwappushservice"
 }
 Out-File -filepath C:\ProgramData\Microsoft\Diagnosis ETLLogsAutoLogger\AutoLogger-Diagtrack-Listener.etl
+force-mkdir "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection"
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name "AllowTelemetry" -Value 0
 
 # Place entries into the hosts file to block known MS telemetry URLs
